d5b67659a83d40558ec9d01af868c90a0d8f7b10
SosnaPL <margonem.kuba@interia.eu>
2022-05-22 15:11:03 +0200

    lighthouse improvements

--- src/components/Introduction/description.tsx
+++ src/components/Introduction/description.tsx
@@ -29,10 +29,14 @@ export const Description = () => {
         />
       </h1>
       <div className="flex items-center pt-6 sm:pt-8 md:pt-10 gap-2 sm:gap-5">
-        <a href="https://github.com/SosnaDev" target="_blank">
+        <a aria-label="Github" href="https://github.com/SosnaDev" target="_blank">
           <LottieWrapper lottie={Github} className={socialIconClass} />
         </a>
-        <a href="https://www.linkedin.com/in/jakub-sosi%C5%84ski-954a7a19b/" target="_blank">
+        <a
+          aria-label="LinkedIn"
+          href="https://www.linkedin.com/in/jakub-sosi%C5%84ski-954a7a19b/"
+          target="_blank"
+        >
           <LottieWrapper lottie={LinkedIn} className={socialIconClass} />
         </a>
       </div>
