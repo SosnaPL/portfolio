21df4ef9d8fff32c828acb2bed9ff5d96219d4d9
SosnaPL <margonem.kuba@interia.eu>
2022-05-28 20:52:15 +0200

    added particles with higher density

--- src/components/Introduction/description.tsx
+++ src/components/Introduction/description.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { jobPositions } from '../constants'
 import Github from '../../public/github.json'
--- src/components/Introduction/introduction.tsx
+++ src/components/Introduction/introduction.tsx
@@ -1,7 +1,6 @@
 import './style.scss'
-import React from 'react'
 
-/* import { ParticlesRenderer } from './../Particles/particles' */
+import { ParticlesRenderer } from './../Particles/particles'
 import { Description } from './description'
 import Coding from '../../public/coding.json'
 import LottieWrapper from '../lottieWrapper'
@@ -9,7 +8,7 @@ import LottieWrapper from '../lottieWrapper'
 export const Introduction = () => {
   return (
     <>
-      {/*  <ParticlesRenderer /> */}
+      <ParticlesRenderer />
       <Description />
       <LottieWrapper lottie={Coding} className={'absolute w-fit bottom-0 right-0'} />
     </>
