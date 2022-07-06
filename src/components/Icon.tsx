882ea28c6c2b54c62bde291ea75e3141b09e95c4
SosnaPL <margonem.kuba@interia.eu>
2022-07-07 01:40:54 +0200

    added props usage

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,10 +1,10 @@
 import LottieWrapper from 'components/LottieWrapper'
 import { IconInfo } from 'types'
 
-export const Icon = ({ name, href, lottie, className }: IconInfo) => {
+export const Icon = (props: IconInfo) => {
   return (
-    <a aria-label={name} href={href} rel="noreferrer" target="_blank">
-      <LottieWrapper lottie={lottie} className={className} />
+    <a aria-label={props.name} href={props.href} rel="noreferrer" target="_blank">
+      <LottieWrapper lottie={props.lottie} className={props.className} />
     </a>
   )
 }
